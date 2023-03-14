587103b5217cbabb235e454cca7dc5d0e83894d5
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-14 22:28:40 +0900

    feat: 자동차 이름 길이 검증 기능 구현

---
+++ src/main/java/racing/ValidatorUtils.java
@@ -0,0 +1,12 @@
+package racing;
+
+public class ValidatorUtils {
+
+    private static final Integer MAX_LENGTH = 5;
+
+    public static void validateLength(String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름이 5자를 초과하였습니다.");
+        }
+    }
+}
---
+++ src/test/java/racing/ValidatorUtilsTest.java
@@ -0,0 +1,14 @@
+package racing;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ValidatorUtilsTest {
+
+    @Test
+    public void 이름이_5자_이상이면_예외() throws Exception {
+        //then
+        Assertions.assertThatThrownBy(() -> ValidatorUtils.validateLength("wallace"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
