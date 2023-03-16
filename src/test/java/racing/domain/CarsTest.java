4f0503fad2115ddb4c4ae147d736e23d0157d81b
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-16 22:35:44 +0900

    refactor: cars test code 수정

--- src/test/java/racing/domain/CarsTest.java
+++ src/test/java/racing/domain/CarsTest.java
@@ -22,12 +22,6 @@ public class CarsTest {
 
     @Test
     public void 결과_반환() throws Exception {
-        //given
-        Cars cars = new Cars(Arrays.asList("gromit", "wallace", "penguin"));
-        //when
-        cars.move("gromit", 4);
-        cars.move("wallace", 3);
-        cars.move("penguin", 0);
         //then
         Map<String, Integer> report = cars.result();
         Assertions.assertThat(report.get("gromit")).isEqualTo(1);
@@ -37,12 +31,6 @@ public class CarsTest {
 
     @Test
     public void 최종_결과() throws Exception {
-        //given
-        Cars cars = new Cars(Arrays.asList("gromit", "wallace", "penguin"));
-        //when
-        cars.move("gromit", 4);
-        cars.move("wallace", 3);
-        cars.move("penguin", 0);
         //then
         Assertions.assertThat(cars.finalResult()).contains("gromit");
         Assertions.assertThat(cars.finalResult()).doesNotContain("wallace", "penguin");
