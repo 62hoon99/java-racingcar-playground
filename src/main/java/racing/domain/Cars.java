ac797ddf2fc322ea29be071a4cd3af70495b2e0f
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-16 22:12:01 +0900

    feat: 최종 결과 반환 기능 구현

--- src/main/java/racing/domain/Cars.java
+++ src/main/java/racing/domain/Cars.java
@@ -1,7 +1,9 @@
 package racing.domain;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Cars {
@@ -22,5 +24,27 @@ public class Cars {
         return racer.keySet()
                 .stream()
                 .collect(Collectors.toMap(key -> key, key -> racer.get(key).currentSpot()));
+    }
+
+    public List<String> finalResult() {
+        Map<String, Integer> resultMap = result();
+        Integer max = getMaxSpot(resultMap);
+        return getWinners(resultMap, max);
+    }
+
+    private List<String> getWinners(Map<String, Integer> resultMap, Integer max) {
+        return resultMap.keySet()
+                .stream()
+                .filter(key -> resultMap.get(key).equals(max))
+                .collect(Collectors.toList());
+    }
+
+    private Integer getMaxSpot(Map<String, Integer> resultMap) {
+        return result().keySet()
+                .stream()
+                .map(resultMap::get)
+                .distinct()
+                .max(Integer::compareTo)
+                .orElseThrow(RuntimeException::new);
     }
 }
--- src/test/java/racing/domain/CarsTest.java
+++ src/test/java/racing/domain/CarsTest.java
@@ -1,6 +1,7 @@
 package racing.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racing.domain.Cars;
 
@@ -8,6 +9,17 @@ import java.util.Arrays;
 import java.util.Map;
 
 public class CarsTest {
+
+    private Cars cars;
+
+    @BeforeEach
+    public void set() {
+        this.cars = new Cars(Arrays.asList("gromit", "wallace", "penguin"));
+
+        cars.move("gromit", 4);
+        cars.move("wallace", 3);
+        cars.move("penguin", 0);
+    }
 
     @Test
     public void 결과_반환() throws Exception {
@@ -22,5 +34,18 @@ public class CarsTest {
         Assertions.assertThat(report.get("gromit")).isEqualTo(1);
         Assertions.assertThat(report.get("wallace")).isEqualTo(0);
         Assertions.assertThat(report.get("penguin")).isEqualTo(0);
+    }
+
+    @Test
+    public void 최종_결과() throws Exception {
+        //given
+        Cars cars = new Cars(Arrays.asList("gromit", "wallace", "penguin"));
+        //when
+        cars.move("gromit", 4);
+        cars.move("wallace", 3);
+        cars.move("penguin", 0);
+        //then
+        Assertions.assertThat(cars.finalResult()).contains("gromit");
+        Assertions.assertThat(cars.finalResult()).doesNotContain("wallace", "penguin");
     }
 }
